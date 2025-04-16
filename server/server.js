e1031452eaa9dea45cbbd60be9f25343ada5ab7b
reevcrona <89569507+reevcrona@users.noreply.github.com>
2025-04-16 18:45:33 +0200

    connected my database to the backend

--- server/server.js
+++ server/server.js
@@ -1,9 +1,32 @@
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 import express from "express";
+import pg from "pg";
+import dotenv from "dotenv";
+const { Client } = pg;
+dotenv.config();
+const db = new Client({
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    host: "localhost",
+    port: 5433,
+    database: "todo",
+});
 const app = express();
 const port = 3000;
-app.get("/", (req, res) => {
-    res.send("yo");
-});
+db.connect();
+app.get("/", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    const result = yield db.query("SELECT * FROM task");
+    console.log(result.rows);
+    res.send(result.rows[0].title);
+}));
 app.listen(port, () => {
     console.log(`Server is up and running on port ${port}`);
 });
--- server/server.ts
+++ server/server.ts
@@ -1,11 +1,28 @@
 import express from "express";
+import pg from "pg";
+import dotenv from "dotenv";
+
+const { Client } = pg;
+dotenv.config();
+
+const db = new Client({
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  host: "localhost",
+  port: 5433,
+  database: "todo",
+});
 
 const app = express();
 
 const port = 3000;
 
-app.get("/", (req, res) => {
-  res.send("yo");
+db.connect();
+
+app.get("/", async (req, res) => {
+  const result = await db.query("SELECT * FROM task");
+  console.log(result.rows);
+  res.send(result.rows[0].title);
 });
 
 app.listen(port, () => {
