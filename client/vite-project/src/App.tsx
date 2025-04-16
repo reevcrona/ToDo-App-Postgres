377a77581096bf65da654dcb8e1c8061ca29b999
reevcrona <89569507+reevcrona@users.noreply.github.com>
2025-04-16 19:17:05 +0200

    added some starter styling

--- client/vite-project/src/App.tsx
+++ client/vite-project/src/App.tsx
@@ -1,5 +1,8 @@
 function App() {
-  return <></>;
+  return (
+    <div className=" w-full h-full flex items-center justify-center">
+      <div className="border-black border-4 bg-transparent w-full max-w-[1000px] min-h-[650px]"></div>
+    </div>
+  );
 }
-
 export default App;
