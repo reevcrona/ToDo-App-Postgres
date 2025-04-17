736cdebf91fbb9e2a334db9c16f36706ca4b43ed
reevcrona <89569507+reevcrona@users.noreply.github.com>
2025-04-17 21:24:11 +0200

    updated tasks to have icons

--- client/vite-project/src/components/Task.tsx
+++ client/vite-project/src/components/Task.tsx
@@ -1,10 +1,20 @@
 import { TaskProps } from "../types/taskProps";
+import { FaPencilAlt } from "react-icons/fa";
+import { RxCross1 } from "react-icons/rx";
 
 function Task({ title }: TaskProps) {
   return (
     <>
-      <div className="bg-white">
+      <div className="bg-white p-4 w-60 mb-4 flex items-center justify-between">
         <h2 className="text-black">{title}</h2>
+        <div className="flex gap-3">
+          <div className="border-2 p-1 cursor-pointer hover:border-amber-600 hover:text-amber-600">
+            <FaPencilAlt />
+          </div>
+          <div className="border-2 p-1 cursor-pointer hover:border-red-600 hover:text-red-600">
+            <RxCross1 />
+          </div>
+        </div>
       </div>
     </>
   );
